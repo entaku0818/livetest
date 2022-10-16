2eff9d29a732e1c939335f7fa6696fe41c5c3099
entaku <entaku19890818@gmail.com>
2022-10-16 23:43:43 +0900

    Initial Commit

---
+++ livetest/livetestApp.swift
@@ -0,0 +1,17 @@
+//
+//  livetestApp.swift
+//  livetest
+//
+//  Created by 遠藤拓弥 on 16.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct livetestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
